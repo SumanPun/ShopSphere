a7ac4d9f2d40d4c05f4b28e3c27a95032bf106ad
suman <sumanpoon27@gmail.com>
2025-01-05 23:55:33 +0545

    added catalog core and infrastructure

---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,89 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository, IBrandRepository, ITypesRepository
+    {
+
+        public ICatalogContext _context { get; }
+        public ProductRepository(ICatalogContext context) 
+        { 
+            _context = context;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context.Products
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products
+                .Find(p => true)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByBrand(string brandName)
+        {
+            return await _context
+                .Products
+                .Find(p => p.Brands.Name.ToLower() == brandName.ToLower())
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            return await _context
+                .Products
+                .Find(p => p.Name.ToLower() == name.ToLower())
+                .ToListAsync();
+        }
+
+        public async Task<Product> CreateProduct(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+            return product;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var deletedProduct = await _context.Products.DeleteOneAsync(p => p.Id == id);
+            return deletedProduct.IsAcknowledged && deletedProduct.DeletedCount > 0;
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updatedProduct = await _context
+                .Products
+                .ReplaceOneAsync(p => p.Id == product.Id, product);
+            return updatedProduct.IsAcknowledged && updatedProduct.ModifiedCount > 0;
+        }
+
+        public async Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            return await _context
+                .Brands
+                .Find(brand => true)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<ProductType>> GetAllTypes()
+        {
+            return await _context
+                .Types
+                .Find(type => true)
+                .ToListAsync();
+        }
+        
+    }
+}
